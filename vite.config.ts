d80492e3cf4f00bc72fab872246855db710e0d3d
niniltp <nhienphan923@gmail.com>
2025-08-28 21:59:49 +0200

    add script deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  base: '/blindtext_b27/',
   assetsInclude: ["**/*.mid"],
   resolve: {
     alias: {
